c25729123e9a460fe28d72ceadf843388dffab95
Anurag-git07 <anuragsathe82@gmail.com>
2026-03-11 19:54:25 +0530

    Create variable.py

---
+++ Day-2/variable.py
@@ -0,0 +1,11 @@
+StudentName="Anurag"
+print(f"StudentName:{StudentName}")
+      
+Age=22
+print(f"Age:{Age}")
+
+City="Ahilyanagar"
+print(f"City:{City}")
+
+Course="MCA"
+print(f"Course:{Course}")
